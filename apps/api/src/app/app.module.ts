f3eede154e866950278b8043a3688a0eca2f5a90
Heiler Nova <heilernova@outlook.com>
2025-01-20 16:54:29 -0500

    feat(api): add ModelsModule to app module for better structure

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@deploy/api/config';
 import { AppService } from './app.service';
+import { ModelsModule } from './models/models.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, ModelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
