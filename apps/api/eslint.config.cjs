70df850d3714ced0eaf717ea24a71c27f63ce064
Heiler Nova <heilernova@outlook.com>
2025-01-20 16:01:29 -0500

    feat(api): enhance ESLint configuration with module boundaries enforcement

--- apps/api/eslint.config.cjs
+++ apps/api/eslint.config.cjs
@@ -1,3 +1,25 @@
 const baseConfig = require('../../eslint.config.cjs');
 
-module.exports = [...baseConfig];
+module.exports = [
+    ...baseConfig,
+    {
+        rules: {
+            '@nx/enforce-module-boundaries': [
+              'error',
+              {
+                enforceBuildableLibDependency: true,
+                allow: [
+                  '^.*/eslint(\\.base)?\\.config\\.[cm]?js$',
+                  "@deploy/api"
+                ],
+                depConstraints: [
+                  {
+                    sourceTag: '*',
+                    onlyDependOnLibsWithTags: ['*'],
+                  },
+                ],
+              },
+            ]
+        }
+    }
+];
