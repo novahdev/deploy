78a54cf05ae313806125215b6e0c6736a6d105df
Heiler Nova <heilernova@outlook.com>
2025-01-20 17:00:52 -0500

    feat(api): add CommonModule for global shared functionality

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@deploy/api/config';
 import { AppService } from './app.service';
 import { ModelsModule } from './models/models.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ConfigModule, ModelsModule],
+  imports: [ConfigModule, ModelsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
