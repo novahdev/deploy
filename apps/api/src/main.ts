18d4cdd6182e808b7b1735e75b82df3c754da903
Heiler Nova <heilernova@outlook.com>
2025-01-20 16:02:58 -0500

    feat(api): integrate ResponseInterceptor and ConfigService in main bootstrap

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,12 +8,15 @@ import { readFileSync } from 'node:fs';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ResponseInterceptor } from '@deploy/api/config';
+import { ConfigService } from '@deploy/api/config';
 
 async function bootstrap() {
   const version = JSON.parse(readFileSync(join("package.json"), 'utf-8')).version;
   process.env.VERSION = version;
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.useGlobalInterceptors(new ResponseInterceptor(app.get(ConfigService)))
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 2025;
   await app.listen(port);
