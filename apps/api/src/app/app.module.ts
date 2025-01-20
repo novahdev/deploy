fd0ec812e54611e8751de7b5851410f8abbcd3db
Heiler Nova <heilernova@outlook.com>
2025-01-20 16:02:52 -0500

    feat(api): update ConfigModule import to use the new path from @deploy/api/config

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@deploy/api/config';
 import { AppService } from './app.service';
-import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [ConfigModule],
