e071fa703b776667ffe67020a3ce91c8af7a3c2b
Heiler Nova <heilernova@outlook.com>
2025-01-21 08:28:52 -0500

    feat(api): add ControllersModule for improved modularity in the application

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { ConfigModule } from '@deploy/api/config';
 import { AppService } from './app.service';
 import { ModelsModule } from './models/models.module';
 import { CommonModule } from './common/common.module';
+import { ControllersModule } from './controllers/controllers.module';
 
 @Module({
-  imports: [ConfigModule, ModelsModule, CommonModule],
+  imports: [ConfigModule, ModelsModule, CommonModule, ControllersModule],
   controllers: [AppController],
   providers: [AppService],
 })
