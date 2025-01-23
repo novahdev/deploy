c841fd8dde418bc763d8565ea04f448e1a8109d8
Heiler Nova <heilernova@outlook.com>
2025-01-23 16:37:01 -0500

    feat(api): update project interfaces to include ProjectStatus and permissions fields

--- apps/api/src/app/models/projects/projects.interfaces.ts
+++ apps/api/src/app/models/projects/projects.interfaces.ts
@@ -1,4 +1,4 @@
-import { Framework, RunningOn, RuntimeEnvironment } from '@deploy/schemas/projects';
+import { Framework, ProjectStatus, Repository, RunningOn, RuntimeEnvironment } from '@deploy/schemas/projects';
 
 export interface IProjectPlaitData {
     id: string;
@@ -18,7 +18,8 @@ export interface IProjectPlaitData {
     repository: string | null;
     env:  string;
     ignore: string;
-    observation: string | null
+    observation: string | null,
+    permissions?: string;
 }
 
 export interface IProject {
@@ -36,10 +37,13 @@ export interface IProject {
     runningOn: RunningOn | null;
     runtimeEnvironment: RuntimeEnvironment | null;
     url: string | null;
-    repository: string | null;
+    repository: Repository | null;
     env: { [key: string]: string };
     ignore: string[];
-    observation: string | null
+    observation: string | null;
+    permissions: string[];
+    status: ProjectStatus;
+    isOnline: boolean;
 }
 
 export type ProjectCreateValues = {
@@ -71,7 +75,23 @@ export type ProjectUpdateValues = {
     runtimeEnvironment?: RuntimeEnvironment | null;
     url?: string | null;
     repository?: string | null;
-    env?:  string;
+    env?:  { [key: string]: string };
+    ignore?: string[];
+    observation?: string | null
+}
+export type ProjectUpdateValuesPlaint = {
+    domain?: string;
+    name?: string;
+    processName?: string;
+    version?: string | null;
+    location?: string;
+    startupFile?: string;
+    framework?: Framework | null;
+    runningOn?: RunningOn | null;
+    runtimeEnvironment?: RuntimeEnvironment | null;
+    url?: string | null;
+    repository?: string | null;
+    env?: string;
     ignore?: string;
     observation?: string | null
 }
