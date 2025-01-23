79d8aa89c172ec27b88a6883862332d50bde7833
Heiler Nova <heilernova@outlook.com>
2025-01-23 16:58:34 -0500

    feat(schemas): update ProjectStatus and ApiProjectRaw interface to enhance project state representation and include repository details

--- packages/schemas/src/projects/project.interfaces.ts
+++ packages/schemas/src/projects/project.interfaces.ts
@@ -3,28 +3,34 @@ import { FRAMEWORK_LIST, RUNNING_ON_LIST, RUNTIME_ENVIRONMENT_LIST } from "./pro
 export type Framework = typeof FRAMEWORK_LIST[number];
 export type RunningOn = typeof RUNNING_ON_LIST[number];
 export type RuntimeEnvironment = typeof RUNTIME_ENVIRONMENT_LIST[number];
-export type ProjectStatus = "online" | "stopping" | "stopped" | "launching" | "errored" | "one-launch-status";
+export type ProjectStatus = "running" | "online" | "stopped" | "errored" | "restarting" | "updating" | "maintenance" | "degraded" | "unknown";
+export interface Repository {
+    type: 'git' | 'svn' | 'mercurial'; // Puedes agregar más tipos si es necesario
+    url: string;
+    branch?: string; // Rama opcional
+    commit?: string; // Último commit opcional
+  }
 
-export interface IProject {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deployAt: Date | null;
-    domain: string;
-    name: string;
-    processName: string;
-    version: string | null; 
-    location: string;
-    startupFile: string;
-    framework: Framework | null;
-    runningOn: RunningOn | null;
-    runtimeEnvironment: RuntimeEnvironment | null;
-    url: string | null;
-    repository: { 
-        type: "git",
-        url: string
-    } | null;
-    env: { [key: string]: string }
-    ignore: string[];
-    observations: string | null;
+export interface ApiProjectRaw {
+    "id": string,
+    "created_at": string,
+    "updated_at": string,
+    "deploy_at": string | null,
+    "status":ProjectStatus,
+    "is_online": boolean,
+    "domain": string,
+    "name": string,
+    "process_name": string,
+    "version": string | null,
+    "location": string,
+    "startup_file": string,
+    "framework": Framework | null,
+    "running_on": RunningOn | null,
+    "runtime_environment": RuntimeEnvironment | null,
+    "url": string | null,
+    "repository": Repository | null,
+    "env": { [key: string]: string },
+    "ignore": string[],
+    "observation": string | null,
+    "permissions": string[]
 }
