2e6df3f8082c614215eb0509bcdf7ae91714d517
Heiler Nova <heilernova@outlook.com>
2025-01-23 09:18:34 -0500

    fix(api): improve clearDir function to handle undefined ignore parameter

--- apps/api/src/app/utils/clear-dir.ts
+++ apps/api/src/app/utils/clear-dir.ts
@@ -3,7 +3,7 @@ import { join } from 'node:path';
 
 export const clearDir = (path: string, ignore?: string[]):  Promise<string[]> => {
     const list: string[] = readdirSync(`${path}`);
-    const fileToDelete: string[]  = list.filter(x => !ignore.some(y => y == x));
+    const fileToDelete: string[] = list.filter(x => !(ignore ?? []).includes(x));
 
     return Promise.all(fileToDelete.map(async item => {
         rmSync(join(path, item), { force: true, recursive: true })
