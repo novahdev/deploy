e0be529d1affd5491565aeb8039b0752df4d011f
Heiler Nova <heilernova@outlook.com>
2025-01-23 09:37:04 -0500

    feat(api): add ProfileController to ControllersModule

--- apps/api/src/app/controllers/controllers.module.ts
+++ apps/api/src/app/controllers/controllers.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { UsersController } from './users/users.controller';
+import { ProfileController } from './profile/profile.controller';
 
 @Module({
-  controllers: [AuthController, UsersController]
+  controllers: [AuthController, UsersController, ProfileController]
 })
 export class ControllersModule {}
