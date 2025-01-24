6a580743a3da70b9a60e39b4d4f323e813ef5baa
Heiler Nova <heilernova@outlook.com>
2025-01-24 18:13:37 -0500

    feat(panel): integrar AuthService e inicializar en AppComponent

--- apps/panel/src/app/app.component.ts
+++ apps/panel/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   imports: [RouterModule],
@@ -8,5 +9,10 @@ import { RouterModule } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private readonly _authService = inject(AuthService);
   title = 'panel';
+
+  constructor(){
+    this._authService.init();
+  }
 }
