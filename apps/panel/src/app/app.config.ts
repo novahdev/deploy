2b436929361bc635f3e88d4189960b6f84519662
Heiler Nova <heilernova@outlook.com>
2025-01-24 15:33:26 -0500

    feat(panel): configurar ng-zorro

--- apps/panel/src/app/app.config.ts
+++ apps/panel/src/app/app.config.ts
@@ -1,10 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { appRoutes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideNzI18n(es_ES),
+    provideAnimationsAsync(),
   ],
 };
