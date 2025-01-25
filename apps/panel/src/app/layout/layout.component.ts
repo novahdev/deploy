d430c979206b548ccb77d8e9002b29095affe548
Heiler Nova <heilernova@outlook.com>
2025-01-25 15:37:56 -0500

    feat(panel): implementar el observable de las sesión para actualizar le nombre en el layout

--- apps/panel/src/app/layout/layout.component.ts
+++ apps/panel/src/app/layout/layout.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzDrawerModule, NzDrawerService } from 'ng-zorro-antd/drawer'
 import { AuthService } from '../auth/auth.service';
 import { DrawerUserMenuComponent } from '../ui/drawer-user-menu/drawer-user-menu.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -13,17 +14,26 @@ import { DrawerUserMenuComponent } from '../ui/drawer-user-menu/drawer-user-menu
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnDestroy {
   private readonly _authService = inject(AuthService);
   private readonly _nzDrawerService = inject(NzDrawerService);
-
+  private  _subscriptionUserChange: Subscription | null = null;
   protected readonly username = signal<string>("");
 
   constructor(){
-    this._authService.sessionChanged.subscribe((session) => { 
-      this.username.set(session?.name || "");
+    this._authService.sessionChanged.subscribe((session) => {
+      if (!session) return;
+      this._subscriptionUserChange = session.valueChanges.subscribe((data) =>{
+        this.username.set(data.name);
+      });
     })
   }
+  ngOnDestroy(): void {
+    if (this._subscriptionUserChange){
+      this._subscriptionUserChange.unsubscribe();
+    }
+  }
+
 
   protected openDrawerUserMenu(): void {
     this._nzDrawerService.create({
