9c52687cc5c1a9fdb4ce7c7150170ae65139d747
Heiler Nova <heilernova@outlook.com>
2025-01-25 15:37:11 -0500

    feat(panel): agregar un método para actualizar los datos de la sesión e implementar un observable para detectar los cambios

--- apps/panel/src/app/auth/session.ts
+++ apps/panel/src/app/auth/session.ts
@@ -1,14 +1,38 @@
+import { BehaviorSubject } from "rxjs";
 
 export class Session {
     public readonly role: "admin" | "collaborator";
     public readonly isAdmin: boolean;
     public readonly name: string;
     public readonly token: string;
+    public readonly _valueChanges: BehaviorSubject<{ role: "admin" | "collaborator", name: string, token: string }>;
 
     constructor(data: { role: "admin" | "collaborator", name: string, token: string }){
         this.role = data.role;
         this.isAdmin = data.role === "admin";
         this.name = data.name;
         this.token = data.token;
+        this._valueChanges = new BehaviorSubject(data);
+    }
+
+    public get valueChanges(){
+        return this._valueChanges.asObservable();
+    }
+
+    public update(data: { name?: string, token?: string }): void {
+        const entries = Object.entries(data);
+        if (entries.length > 0){
+            const properties: PropertyDescriptorMap = {};
+            entries.forEach(([key, value]) => { 
+                properties[key] = { value, writable: true }
+            });
+            Object.defineProperties(this, properties);
+            localStorage.setItem("app-token", window.btoa(JSON.stringify({
+                role: this.role,
+                name: this.name,
+                token: this.token
+            })));
+            this._valueChanges.next({ role: this.role, name: this.name, token: this.token });
+        }
     }
 }
