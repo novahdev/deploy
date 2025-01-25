1547f6c5f6eed806f79c6f1d37789970c018e86e
Heiler Nova <heilernova@outlook.com>
2025-01-25 09:21:08 -0500

    feat(panel): agregar el proveedor HttpClient a las configuraciones de la aplicación

--- apps/panel/src/app/app.config.ts
+++ apps/panel/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { es_ES, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
     provideNzI18n(es_ES),
     provideAnimationsAsync(),
   ],
