a5ec69090997418569e78f1264611075dd9028a3
Heiler Nova <heilernova@outlook.com>
2025-01-25 11:29:19 -0500

    feat(panel): agregar la propiedad name a la clases Sesión

--- apps/panel/src/app/auth/session.ts
+++ apps/panel/src/app/auth/session.ts
@@ -2,11 +2,13 @@
 export class Session {
     public readonly role: "admin" | "collaborator";
     public readonly isAdmin: boolean;
+    public readonly name: string;
     public readonly token: string;
 
     constructor(data: { role: "admin" | "collaborator", name: string, token: string }){
         this.role = data.role;
         this.isAdmin = data.role === "admin";
+        this.name = data.name;
         this.token = data.token;
     }
 }
