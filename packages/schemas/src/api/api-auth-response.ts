b55ba54146d05f4562f78343d782615135ace6d7
Heiler Nova <heilernova@outlook.com>
2025-01-25 16:18:10 -0500

    feat(api): ampliar la respuesta de autenticación con la estructura de tokens de acceso

--- packages/schemas/src/api/api-auth-response.ts
+++ packages/schemas/src/api/api-auth-response.ts
@@ -1,9 +1,21 @@
-import { ApiResponseBase } from "./api-response-base.interfaces";
+import { ApiResponse } from "./api-response-base.interfaces";
 
-export interface ApiAuthResponse extends ApiResponseBase {
+export interface ApiAuthResponse extends ApiResponse {
     data: {
         role: "admin" | "collaborator",
         name: string,
         token: string,
     }
+}
+export interface ApiAccessTokensResponse extends ApiResponse {
+    data: {
+        id: string,
+        created_at: string,
+        type: "cli" | "web",
+        hostname: string,
+        ip: string,
+        device: "desktop" | "mobile" | "tablet",
+        platform: string | null,
+        exp: string | null
+    }[]
 }
