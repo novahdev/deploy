00ed5ff9c77c27ce20a59fc526ff7a2acac1c2ba
Heiler Nova <heilernova@outlook.com>
2025-01-27 09:29:12 -0500

    feat(panel): agregar el componente app-profile-securuty a la página del perfil de usuario

--- apps/panel/src/app/pages/profile-page/profile-page.component.ts
+++ apps/panel/src/app/pages/profile-page/profile-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ProfileInfoComponent } from '@deploy/panel/ui/profile-info/profile-info.component';
+import { ProfileSecurityComponent } from '@deploy/panel/ui/profile-security/profile-security.component';
 
 @Component({
   selector: 'app-profile-page',
   imports: [
-    ProfileInfoComponent
+    ProfileInfoComponent,
+    ProfileSecurityComponent
   ],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
