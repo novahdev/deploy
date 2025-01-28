c4a5d62fa87368462b7826249eb0d28368f27993
Heiler Nova <heilernova@outlook.com>
2025-01-28 16:46:10 -0500

    feat(api): agregar manejo de errores en `apiInterceptor`para solicitudes no autorizadas

--- apps/panel/src/app/config/api.interceptor.ts
+++ apps/panel/src/app/config/api.interceptor.ts
@@ -1,9 +1,12 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../auth/auth.service';
 import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
   let headers = req.headers;
 
   const session = authService.session;
@@ -11,5 +14,13 @@ export const apiInterceptor: HttpInterceptorFn = (req, next) => {
     headers = headers.set("X-App-Token", session.token);
   }
 
-  return next(req.clone({ headers }));
+  return next(req.clone({ headers })).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(["/login"]);
+      }
+      return throwError(() => error)
+    })
+  );
 };
