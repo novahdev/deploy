6e857b82ee5ba031b1e8bc967a09af00a5dede80
Heiler Nova <heilernova@outlook.com>
2025-01-30 15:27:16 -0500

    feat(api): add ServeStaticModule for serving static files based on environment

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@deploy/api/config';
 import { AppService } from './app.service';
 import { ModelsModule } from './models/models.module';
 import { CommonModule } from './common/common.module';
 import { ControllersModule } from './controllers/controllers.module';
+import { join } from 'node:path';
 
 @Module({
-  imports: [ConfigModule, ModelsModule, CommonModule, ControllersModule],
+  imports: [
+    ConfigModule,
+    ModelsModule,
+    CommonModule, ControllersModule,
+    ServeStaticModule.forRoot({
+      rootPath: process.env.NODE_ENV === "development" ? join(__dirname, '../panel/browser') : join('./public/browser'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
